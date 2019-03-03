12117c5282f0f962cc9336e1550f421f1c2168c2
MatiFilozof <32573962+MatiFilozof@users.noreply.github.com>
2019-03-03 18:21:15 +0100

    dodane generowanie PD2

--- scripts/torontoDataGetter.R
+++ scripts/torontoDataGetter.R
@@ -1,9 +1,49 @@
 library(jsonlite)
 library(purrr)
 library(reader)
+library(mlr)
+library(digest)
 
 # MOCNO przekształcony kod funkcji summary.default
 # właściwie to napisany od podstaw
+
+set.seed(1)
+
+getFilledCode <- function(name, target, learner, measures, pars, hash, isRegr) {
+  return(paste0("#:# libraries
+library(digest)
+library(mlr)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- read.csv(\"https://raw.githubusercontent.com/mini-pw/2019L-WarsztatyBadawcze_zbiory/master/toronto_", name, "/", name, ".csv\")
+head(dataset)
+
+#:# preprocessing
+head(dataset)
+
+#:# model
+task = make", ifelse(isRegr, "Regr", "Classif"), "Task(id = \"", name, "\", data = dataset, target = \"", target, "\")
+lrn = makeLearner(\"", learner, "\", par.vals = ", deparse(pars), ifelse(isRegr, "", ", predict.type = \"prob\""), ")
+
+#:# hash 
+#:# ", hash, "
+hash <- digest(lrn)
+hash
+
+#:# audit
+cv <- makeResampleDesc(\"CV\", iters = 5)
+r <- resample(lrn, task, cv, measures = ", paste0("list(", paste(simplify(sapply(measures, "[", "id")), collapse = ", "), ")"), ")
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0(\"sessionInfo.txt\"))
+sessionInfo()
+sink()"))
+}
 
 dummary <- function (object, colname, ...) {
   isCat <- is.factor(object) || is.logical(object)
@@ -43,7 +83,15 @@ dummary <- function (object, colname, ...) {
   return(props)
 }
 
-getTorontoDataset <- function(name, added_by) {
+processParams <- function(defaults, hypers) {
+  listNames <- names(defaults$pars)
+  defaults <- sapply(defaults$pars, "[", "default")
+  names(defaults) <- listNames
+  params <- modifyList(defaults, hypers)
+  return(defaults)
+}
+
+getTorontoData <- function(name, saveToCsv = FALSE) {
   download.file(paste0("https://www.cs.toronto.edu/pub/neuron/delve/data/tarfiles/", name, ".tar.gz"), "tmp.tar.gz")
   untar("tmp.tar.gz")
   table <- read.table(paste0("./", name, "/Dataset.data.gz"))
@@ -53,12 +101,22 @@ getTorontoDataset <- function(name, added_by) {
   writeLines(res, "tmp.txt")
   cols <- read.table("tmp.txt", comment.char = "u")$V2
   file.remove("tmp.txt")
-  colnames(table) <- cols
+  file.remove("tmp.tar.gz")
+  colnames(table) <- make.names(cols, unique = TRUE)
+  # here can be inserted dealing with missing data and all that stuff
+  if(saveToCsv) {
+    write.csv(table, paste0(name, ".csv"), row.names = FALSE)
+  }
+  return(table)
+}
+
+createTorontoDataset <- function(name, added_by) {
+  table <- getTorontoData(name)
   
   # część kodu poniżej inspirowana: https://stackoverflow.com/questions/39168484/r-summary-to-parsable-format-preferable-json
   
   variables <- table %>% imap(dummary)
-  notYetJson <- list(id = paste0("toronto_", name),
+  toDataJson <- list(id = paste0("toronto_", name),
                      added_by = added_by,
                      date = format(Sys.Date(), "%d-%m-%Y"),
                      name = name,
@@ -69,7 +127,70 @@ getTorontoDataset <- function(name, added_by) {
                      number_of_missing_values = sum(is.na(table)),
                      number_of_instances_with_missing_values = sum(apply(table, 1, function(x) any(is.na(x)))),
                      variables = variables)
-  json <- list(notYetJson) %>% toJSON(auto_unbox = TRUE, pretty = TRUE)   # convert to JSON, make it legible
+  dataJson <- list(toDataJson) %>% toJSON(auto_unbox = TRUE, pretty = TRUE, null = "null")
   dir.create(paste0("toronto_", name))
-  write(json, paste0("./toronto_", name, "/dataset.json"))
+  write(dataJson, paste0("./toronto_", name, "/dataset.json"))
+}
+
+createTorontoTask <- function(name, added_by, target, learner, measures = list(acc), measureNames = "ACC", pars = list()) {
+  stopifnot(length(measures) == length(measureNames))
+  table <- getTorontoData(name)
+  type <- ifelse(substr(learner, 1, 4) == "clas", "classification", "regression")
+  isRegr <- ifelse(substr(learner, 1, 4) == "clas", FALSE, TRUE)
+  toTaskJson <- list(id = paste0("toronto_", name),
+                     added_by = added_by,
+                     date = format(Sys.Date(), "%d-%m-%Y"),
+                     dataset_id = name,
+                     type = type,
+                     target = target)
+  taskJson <- list(toTaskJson) %>% toJSON(auto_unbox = TRUE, pretty = TRUE, null = "null")
+  dir.create(paste0("toronto_", name))
+  dir.create(paste0("toronto_", name, "/", type, "_", target))
+  write(taskJson, paste0("./toronto_", name, "/", type, "_", target, "/task.json"))
+  
+  if (type == "classification") {
+    class_task <- makeClassifTask(id = paste0("toronto_", name), data = table, target = target)
+    class_lrn <- makeLearner(learner, par.vals = pars, predict.type = "prob")
+  }
+  else {
+    class_task <- makeRegrTask(id = paste0("toronto_", name), data = table, target = target)
+    class_lrn <- makeLearner(learner, par.vals = pars)
+  }
+  
+  cv <- makeResampleDesc("CV", iters = 5)
+  r <- resample(class_lrn, class_task, cv, measures = measures)
+  MSE <- r$aggr
+  names(MSE) <- measureNames
+  
+  hash <- digest(learner)
+  toAuditJson <- list(id = paste0("audit_", hash),
+                      added_by = added_by,
+                      date = format(Sys.Date(), "%d-%m-%Y"),
+                      model_id = hash,
+                      task_id = paste0(type, "_", target),
+                      dataset_id = paste0("toronto_", name),
+                      performance = as.list(MSE))
+  auditJson <- list(toAuditJson) %>% toJSON(auto_unbox = TRUE, pretty = TRUE, null = "null")
+  dir.create(paste0("toronto_", name, "/", type, "_", target, "/", hash))
+  write(auditJson, paste0("toronto_", name, "/", type, "_", target, "/", hash, "/audit.json"))
+  
+  params <- processParams(getParamSet(class_lrn), getHyperPars(class_lrn))
+  
+  variables <- table %>% imap(dummary)
+  toModelJson <- list(id = hash,
+                      added_by = added_by,
+                      date = format(Sys.Date(), "%d-%m-%Y"),
+                      task_id = paste0(type, "_", target),
+                      dataset_id = paste0("toronto_", name),
+                      parameters = params,
+                      preprocessing = variables)
+  modelJson <- list(toModelJson) %>% toJSON(auto_unbox = TRUE, pretty = TRUE, null = "null")
+  write(modelJson, paste0("toronto_", name, "/", type, "_", target, "/", hash, "/model.json"))
+  
+  write(getFilledCode(name, target, learner, measures, pars, hash, isRegr),
+        paste0("toronto_", name, "/", type, "_", target, "/", hash, "/code.R"))
+  # z tym poniżej poczekaj do pojawienia się datasetu na githubie
+  # EDIT: jednak nie, jednak należy to robić ręcznie; automatyczne wykonanie kodu dodaje niechciane linie do sessionInfo.txt
+  # setwd(paste0("toronto_", name, "/", type, "_", target, "/", hash, "/"))
+  # source("code.R", echo = TRUE)
 }
