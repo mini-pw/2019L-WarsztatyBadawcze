cd790e2f888f731afc56309b5be6ceb2402e2eeb
WojciechKretowicz <wojtekkretowicz@gmail.com>
2019-03-06 15:34:37 +0100

    Zadanie domowe 2

---
+++ models/openml_glass/regression_Mg/a5a078898703d7e1e7b6b3322b4f0ac8/code.py
@@ -0,0 +1,45 @@
+import openml
+import numpy as np
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Loading data
+
+datasetOpenmlId = 41
+
+
+dataset = openml.datasets.get_dataset(datasetOpenmlId)
+
+(X, y, categorical, names) = dataset.get_data(
+    target=dataset.default_target_attribute,
+    return_categorical_indicator=True,
+    return_attribute_names=True,
+    include_ignore_attributes=True
+)
+
+vals = {}
+for i, name in enumerate(names):
+    vals[name] = X[:, i]
+vals[dataset.default_target_attribute] = y
+df = pd.DataFrame(vals)
+
+X = df.drop('Mg', axis=1)
+y = df.loc[:, 'Mg']
+
+names.remove("Mg")
+
+# Model
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=777)
+
+mdl = LinearRegression()
+mdl.fit(X_train, y_train)
+
+# Audit
+
+y_pred = mdl.predict(X_test)
+
+print(mean_squared_error(y_test, y_pred))
