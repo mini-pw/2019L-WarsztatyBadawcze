e3f1448737ed25adc1f48d729dc5b78f0e2a4abc
mckraqs <mckraqs@gmail.com>
2019-03-13 19:15:55 +0100

    Laby4 - Polakowski Mateusz

--- models/openml_skin-segmentation/regression_V1/8c3c97c4b2ef4c087f94b686d3259a58/code.R
+++ models/openml_skin-segmentation/regression_V1/8c3c97c4b2ef4c087f94b686d3259a58/code.R
@@ -2,9 +2,10 @@
 library(digest)
 library(OpenML)
 library(mlr)
+library(gbm)
 
 #:# config
-set.seed(997)
+set.seed(123, "L'Ecuyer")
 
 #:# data
 skin_seg <- getOMLDataSet(data.id = 1502)
@@ -15,18 +16,18 @@ head(skin_seg)
 
 #:# model
 regr_task = makeRegrTask(data = skin_seg, target = "V1")
-regr_lrn = makeLearner("regr.rpart", par.vals = list(maxdepth = 5))
+regr_lrn = makeLearner("regr.gbm")
 
 #:# hash 
-#:# 6284e089672fe91b703313fcc38de19b
+#:# 8c3c97c4b2ef4c087f94b686d3259a58
 hash <- digest(regr_lrn)
 hash
 
 #:# audit
 cv <- makeResampleDesc("CV", iters = 5)
-r <- resample(regr_lrn, regr_task, cv, measures = list(mse))
-MSE <- r$aggr
-MSE
+r <- resample(regr_lrn, regr_task, cv, measures = list(mse, mae, rmse, rsq))
+er <- r$aggr
+er
 
 #:# session info
 sink(paste0("sessionInfo.txt"))
