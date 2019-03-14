6f28d2a98586acd0b1486dc21ce4a180d2fa4147
Hubert <h.baniecki@student.mini.pw.edu.pl>
2019-03-14 23:58:16 +0100

    PD4

---
+++ models/kaggle_Toy-Dataset/classification_Gender/def026ac76b696bfc9aea433d9a1e581/code.R
@@ -0,0 +1,41 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(readr)
+library(dplyr)
+
+#:# config
+set.seed(1)
+
+#:# data
+enc <- guess_encoding("toy_dataset.csv", n_max = 10000)[[1]]
+df <- as.data.frame(read_csv("toy_dataset.csv", locale = locale(encoding = enc[1])))
+head(df)
+
+#:# preprocessing
+df <- select(df, -Number)
+df$City <- as.factor(df$City)
+df$Illness <- as.factor(df$Illness)
+head(df)
+
+#:# model
+classif_task <- makeClassifTask(id = "task", data = df, target = "Gender")
+classif_lrn <- makeLearner("classif.gbm", predict.type = "prob",
+                           par.vals = list(distribution = "bernoulli", n.trees=130, interaction.depth=4,
+                                           shrinkage = 0.1))
+
+#:# hash 
+#:# def026ac76b696bfc9aea433d9a1e581
+hash <- digest(list(classif_task, classif_lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- resample(classif_lrn, classif_task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+measure <- r$aggr
+measure
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
