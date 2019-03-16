bba78bbb0831b48e99dd70aaa913cf5e8a20c09d
k-sap <karlo975@hotmail.com>
2019-03-16 20:42:56 +0100

    regresja openml_glass

--- models/openml_glass/regression_Mg/0b05bc74eca65d46db8228b61f7fc032/code.R
+++ models/openml_glass/regression_Mg/0b05bc74eca65d46db8228b61f7fc032/code.R
@@ -22,14 +22,14 @@ task <- createDummyFeatures(obj = task)
 learner <- makeLearner("regr.evtree")
 
 #:# hash
-#:# a83d5cf9f3c9dc688b8e1f64b2e92047
+#:# 0b05bc74eca65d46db8228b61f7fc032
 list_to_hash <-list(task, learner)
 hash <- digest(list_to_hash)
 hash
 
 #:# audit
 cv <- makeResampleDesc("CV", iters = 5)
-r <- resample(classif_lrn, classif_task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+r <- resample(learner, task, cv, measures = list(mse, rmse, mae, rsq))
 ACC <- r$aggr
 ACC
 
