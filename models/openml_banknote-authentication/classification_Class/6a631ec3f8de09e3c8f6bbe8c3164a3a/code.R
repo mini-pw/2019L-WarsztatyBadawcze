08959e70e17fc8b69771e49aac41e71e8d528583
Pastuszka <32496556+Pastuszka@users.noreply.github.com>
2019-03-17 13:47:13 +0100

    Pd4  + vat
    based on work of D. Rafacz (WUT, 2019)

---
+++ models/openml_banknote-authentication/classification_Class/6a631ec3f8de09e3c8f6bbe8c3164a3a/code.R
@@ -0,0 +1,70 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "banknote-authentication")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.ranger", par.vals = structure(list(num.trees = 1000, num.random.splits = 5L), .Names = c("num.trees", , predict.type = "prob")
+
+#:# hash
+#:# 6a631ec3f8de09e3c8f6bbe8c3164a3a
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "banknote-authentication")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.ranger", par.vals = "num.random.splits")), predict.type = "prob")
+
+#:# hash
+#:# 6a631ec3f8de09e3c8f6bbe8c3164a3a
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
