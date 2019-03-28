3ef30cb5628dddf88e57dbe6e50809e9e9b33259
MatiFilozof <matibakala@gmail.com>
2019-03-28 23:08:48 +0100

    openml_no2; 61 modeli

---
+++ models/openml_no2/classification_binaryClass/76f8db16c13e1e5d42af4c21200729cd/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "no2")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "binaryClass")
+lrn = makeLearner("classif.qda", par.vals = list(method = "moment", predict.method = "debiased"), predict.type = "prob")
+
+#:# hash
+#:# 76f8db16c13e1e5d42af4c21200729cd
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
