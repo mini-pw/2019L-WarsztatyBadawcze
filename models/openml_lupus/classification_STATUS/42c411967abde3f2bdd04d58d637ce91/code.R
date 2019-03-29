98052acb67fac40c766931ce4ed71c98ad6d9c2b
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2019-03-29 17:41:31 +0100

    Add files via upload

---
+++ models/openml_lupus/classification_STATUS/42c411967abde3f2bdd04d58d637ce91/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "lupus")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "STATUS")
+lrn = makeLearner("classif.ranger", par.vals = list(num.trees = 1000, num.random.splits = 2L), predict.type = "prob")
+
+#:# hash
+#:# 42c411967abde3f2bdd04d58d637ce91
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
