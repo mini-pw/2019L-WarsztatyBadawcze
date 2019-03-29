70b9278879206f7b1a8febd6b1cdf9723c66523e
witoldmerkel <32493195+witoldmerkel@users.noreply.github.com>
2019-03-29 23:17:54 +0100

    Faza 2

---
+++ models/openml_Satellite/classification_Target/3feb63fc5bac187d8479640030ee1144/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "Satellite")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Target")
+lrn = makeLearner("classif.ada", par.vals = list(loss = "logistic", iter = 20, nu = 1), predict.type = "prob")
+
+#:# hash
+#:# 3feb63fc5bac187d8479640030ee1144
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
