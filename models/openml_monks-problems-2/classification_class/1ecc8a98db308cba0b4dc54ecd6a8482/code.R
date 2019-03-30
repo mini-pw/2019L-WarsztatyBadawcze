2f6f86a960c1afbf4d47afd84cbdf66432669180
hbaniecki <h.baniecki@student.mini.pw.edu.pl>
2019-03-30 22:34:46 +0100

    pd6

---
+++ models/openml_monks-problems-2/classification_class/1ecc8a98db308cba0b4dc54ecd6a8482/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "monks-problems-2")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class")
+lrn = makeLearner("classif.gbm", par.vals = list(n.trees = 10, shrinkage = 0.002, bag.fraction = 0.3), predict.type = "prob")
+
+#:# hash
+#:# 1ecc8a98db308cba0b4dc54ecd6a8482
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
---
+++ models/openml_monks-problems-2/classification_class/4c8d7d5be54f000cf38b36e3710db2ce/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "monks-problems-2")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class")
+lrn = makeLearner("classif.gbm", par.vals = list(n.trees = 10, shrinkage = 0.05, bag.fraction = 0.3), predict.type = "prob")
+
+#:# hash
+#:# 4c8d7d5be54f000cf38b36e3710db2ce
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
