85c8130c723a17e4bfa42b3871c2e1cc65e824f1
Wojciech Bogucki <boguckiw@student.mini.pw.edu.pl>
2019-03-31 14:13:43 +0200

    PD6

---
+++ models/openml_phoneme/classification_Class/dd549d3091f543419a8d6224a3efac2f/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "phoneme")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.cforest", par.vals = list(ntree = 1000, testtype = "Univariate"), predict.type = "prob")
+
+#:# hash
+#:# dd549d3091f543419a8d6224a3efac2f
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
