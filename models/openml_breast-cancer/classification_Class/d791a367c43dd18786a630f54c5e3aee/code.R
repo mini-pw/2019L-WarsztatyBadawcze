05e3f8bdd9325e672f2bd2d0cdc714d14d686519
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2019-03-31 15:04:28 +0200

    PD6

---
+++ models/openml_breast-cancer/classification_Class/d791a367c43dd18786a630f54c5e3aee/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "breast-cancer")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.rpart", par.vals = list(minsplit = 10, maxcompete = 4, maxdepth = 10), predict.type = "prob")
+
+#:# hash
+#:# d791a367c43dd18786a630f54c5e3aee
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
