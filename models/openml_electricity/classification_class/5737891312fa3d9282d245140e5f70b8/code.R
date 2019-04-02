390d8f8f6d30bec7a1fb7d8c78d24e34d69ee421
pysiakk <k.pysiak@student.mini.pw.edu.pl>
2019-04-02 23:21:26 +0200

    PD6 Karol Pysiak

---
+++ models/openml_electricity/classification_class/5737891312fa3d9282d245140e5f70b8/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "electricity")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class")
+lrn = makeLearner("classif.nnet", par.vals = list(size = 5, maxit = 20), predict.type = "prob")
+
+#:# hash
+#:# 5737891312fa3d9282d245140e5f70b8
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
