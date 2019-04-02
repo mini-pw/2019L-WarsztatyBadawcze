c422d54959eba4cccb7db141580821e4f9ef35c2
Pastuszka <m.pastuszka@student.mini.pw.edu.pl>
2019-04-02 19:17:17 +0200

    pd

---
+++ models/openml_MagicTelescope/classification_class./58d088c2bccae932c0441578445152c0/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "MagicTelescope")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class.")
+lrn = makeLearner("classif.bartMachine", par.vals = list(num_trees = 10, seed = 420, alpha = 0.95), predict.type = "prob")
+
+#:# hash
+#:# 58d088c2bccae932c0441578445152c0
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
---
+++ models/openml_MagicTelescope/classification_class./6ffecca113eedf2f4140d0af73a3f0d4/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "MagicTelescope")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class.")
+lrn = makeLearner("classif.glmnet", par.vals = list(alpha = 1, nlambda = 400), predict.type = "prob")
+
+#:# hash
+#:# 6ffecca113eedf2f4140d0af73a3f0d4
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
