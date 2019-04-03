61be4e5bce6fd7623ad5703aed6dbeb8ced1f466
k-sap <karlo975@hotmail.com>
2019-04-03 15:13:47 +0200

    PD6 - models

---
+++ models/openml_ozone-level-8hr/classification_Class/f14215881e4bbcbc1c87b77a397e9894/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "ozone-level-8hr")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.ctree", par.vals = list(), predict.type = "prob")
+
+#:# hash
+#:# f14215881e4bbcbc1c87b77a397e9894
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
---
+++ models/openml_ozone-level-8hr/classification_Class/fbdd4239c5d0548725aeec044f944014/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "ozone-level-8hr")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.svm", par.vals = list(gamma = 8L, tolerance = 1L, fitted = TRUE, shrinking = TRUE), predict.type = "prob")
+
+#:# hash
+#:# fbdd4239c5d0548725aeec044f944014
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
