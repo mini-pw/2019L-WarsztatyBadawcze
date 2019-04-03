372ef76166e53751022cdc9955f1346999672c99
pmizinski <mizinski94@gmail.com>
2019-04-03 14:18:12 +0200

    pd6

---
+++ models/openml_blood-transfusion-service-center/classification_Class/772a534e21a93e73d07afdec7b512f8d/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "blood-transfusion-service-center")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.neuralnet", par.vals = list(hidden = 1L, rep = 3L), predict.type = "prob")
+
+#:# hash
+#:# 772a534e21a93e73d07afdec7b512f8d
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
---
+++ models/openml_blood-transfusion-service-center/classification_Class/a4ee3b01fb0a669f95c567b9a7a4f409/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "blood-transfusion-service-center")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "Class")
+lrn = makeLearner("classif.ranger", par.vals = list(num.trees = 1000, num.random.splits = 2L), predict.type = "prob")
+
+#:# hash
+#:# a4ee3b01fb0a669f95c567b9a7a4f409
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
