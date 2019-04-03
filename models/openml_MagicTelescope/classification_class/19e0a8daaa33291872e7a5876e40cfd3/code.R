cf137f5c3ab5c60ea1d6c084b6bac8b3eead697a
jusira <alicjagosiewska@gmail.com>
2019-04-03 09:07:36 +0200

    Update models

---
+++ models/openml_MagicTelescope/classification_class/19e0a8daaa33291872e7a5876e40cfd3/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "MagicTelescope")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class.")
+lrn = makeLearner("classif.ada", par.vals = list(type = "gentle", iter = 20), predict.type = "prob")
+
+#:# hash
+#:# 19e0a8daaa33291872e7a5876e40cfd3
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
