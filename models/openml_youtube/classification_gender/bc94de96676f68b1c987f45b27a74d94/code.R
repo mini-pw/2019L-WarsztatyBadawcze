a1393cb49136a9fcf09089bc9617b0b2186db541
granatb <32493624+granatb@users.noreply.github.com>
2019-04-03 16:30:11 +0200

    pd6

---
+++ models/openml_youtube/classification_gender/bc94de96676f68b1c987f45b27a74d94/code.R
@@ -0,0 +1,70 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "youtube")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "gender")
+lrn = makeLearner("classif.randomForest", par.vals = structure(list(ntree = 100, nodesize = 1), .Names = c("ntree", , predict.type = "prob")
+
+#:# hash
+#:# bc94de96676f68b1c987f45b27a74d94
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "youtube")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "gender")
+lrn = makeLearner("classif.randomForest", par.vals = "nodesize")), predict.type = "prob")
+
+#:# hash
+#:# bc94de96676f68b1c987f45b27a74d94
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
