823f244a7af27a9a4beb04c9ffe99698f0090ef7
Adam Rydelek <32574203+Adamoso@users.noreply.github.com>
2019-04-03 15:27:14 +0200

    PD

---
+++ models/openml_churn/classification_class/4f54fa3e624274417e4242a9e4915cbb/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "churn")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class")
+lrn = makeLearner("classif.gbm", par.vals = list(n.trees = 100, n.minobsinnode = 15), predict.type = "prob")
+
+#:# hash
+#:# 4f54fa3e624274417e4242a9e4915cbb
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
