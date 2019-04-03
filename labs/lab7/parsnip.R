0a7bc256091dbfff18f882047ac83c1dd05e3d97
jusira <alicjagosiewska@gmail.com>
2019-04-03 14:52:52 +0200

    Update parsnip.R

--- labs/lab7/parsnip.R
+++ labs/lab7/parsnip.R
@@ -20,7 +20,10 @@ model <- rand_forest(mode = "regression") %>%
   set_engine("ranger", seed = 63233) %>%
   set_args(trees = 2000, mtry = 4) %>%
   fit(drinks~., data = liver)
+# List of models
+# https://tidymodels.github.io/parsnip/articles/articles/Models.html
 
+# prediction
 predict(model, test)
 
 
