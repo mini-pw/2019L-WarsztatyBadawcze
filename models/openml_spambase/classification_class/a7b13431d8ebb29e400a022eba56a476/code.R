ae3cd726cbe607675fe648f6f9c249d4525f529e
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-04-10 13:18:18 +0200

    PD6

---
+++ models/openml_spambase/classification_class/a7b13431d8ebb29e400a022eba56a476/code.R
@@ -0,0 +1,35 @@
+#:# libraries
+library(digest)
+library(mlr)
+library(OpenML)
+library(farff)
+
+#:# config
+set.seed(1)
+
+#:# data
+dataset <- getOMLDataSet(data.name = "spambase")
+head(dataset$data)
+
+#:# preprocessing
+head(dataset$data)
+
+#:# model
+task = makeClassifTask(id = "task", data = dataset$data, target = "class")
+lrn = makeLearner("classif.xgboost", par.vals = list(gamma = 0, max_depth = 3, eta = 0.6), predict.type = "prob")
+
+#:# hash
+#:# a7b13431d8ebb29e400a022eba56a476
+hash <- digest(list(task, lrn))
+hash
+
+#:# audit
+cv <- makeResampleDesc("CV", iters = 5)
+r <- mlr::resample(lrn, task, cv, measures = list(acc, auc, tnr, tpr, ppv, f1))
+ACC <- r$aggr
+ACC
+
+#:# session info
+sink(paste0("sessionInfo.txt"))
+sessionInfo()
+sink()
