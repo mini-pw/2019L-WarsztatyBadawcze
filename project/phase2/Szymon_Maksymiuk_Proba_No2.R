a9e0116579e2b3edd47284ca8d3df5eb77e15023
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-06-12 12:05:28 +0200

    Phase2

---
+++ project/phase2/Szymon_Maksymiuk_Proba_No2.R
@@ -0,0 +1,26 @@
+library(mlr)
+library(dplyr)
+
+
+train <- select(read.csv("train.csv", sep=";"), -c(Zuzanna, Julia, Oliwia, Maria, Nikola, Gabriela))
+test <- select(read.csv("WarsztatyBadawcze_test.csv", sep=";"), -c(Zuzanna, Julia, Oliwia, Maria, Nikola, Gabriela))
+
+train$Aleksandra <- as.factor(train$Aleksandra)
+test$Aleksandra <- as.factor(test$Aleksandra)
+train$Natalia <- as.factor(train$Natalia)
+test$Natalia <- as.factor(test$Natalia)
+train$Nadia <- as.factor(train$Nadia)
+test$Nadia <- as.factor(test$Nadia)
+train$Pola <- as.factor(train$Pola)
+test$Pola <- as.factor(test$Pola)
+train$Liliana <- as.factor(train$Liliana)
+test$Liliana <- as.factor(test$Liliana)
+
+task <- makeClassifTask(id = "wb", data = train, target = "Y")
+lrn <- makeLearner("classif.ranger", predict.type = "prob", par.vals = list(num.trees = 1600, mtry = 3, min.node.size = 8))
+
+model <- train(lrn, task)
+pred <- predict(model, newdata=test)
+write.csv(pred$data$prob.TRUE, "preds.csv", row.names = FALSE)
+
+
